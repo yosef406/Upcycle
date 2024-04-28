5af5f511d2c49c2d3457f466c8e97b2d3c13a4a0
Yosef Awad <yosef.awad.1@gmail.com>
2024-04-29 00:46:43 +0300

    disableDB

--- servers/http_server.js
+++ servers/http_server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
-const Products = require("../db/database");
+// const Products = require("../db/database");
 
 // Define routes for the Express.js server
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", (req, res) => {
-  Product.find()
-    .then((products) => {
-      res.send(products);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error");
-    });
-});
+// app.get("/products", (req, res) => {
+//   Product.find()
+//     .then((products) => {
+//       res.send(products);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error");
+//     });
+// });
 
 app.post("/add-to-cart", (req, res) => {
   const { productId } = req.body;
